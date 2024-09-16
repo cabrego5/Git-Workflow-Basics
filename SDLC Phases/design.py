b2264d779bbec4a9de21c4ce1e5cf351d221e72f
Saul Matthew Ruiz <mattruiz@Sauls-MacBook-Pro.local>
2024-09-15 19:13:26 -0500

    Adding design file

--- SDLC Phases/design.py
+++ SDLC Phases/design.py
@@ -1,7 +1,7 @@
 """
 The design phase of the software development lifecycle is where the software project is outlined. 
 In present day projects a software architect will create the flow and design of the system before rookie devlopers add code.
- This could include wireframes or other means of showcasing the project design.
+This could include wireframes or other means of showcasing the project design.
 """
 print("Hola design phase")
 
